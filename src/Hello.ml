eb1e5fea179d96fd9b8eb431051d649f9b83f824
InfiniteSwerve <jackofthestrand@gmail.com>
2023-10-22 22:35:38 -0700

    modified towards tonedrop

--- src/Hello.ml
+++ src/Hello.ml
@@ -1,1 +1,12 @@
-let () = Js.log ("Hello " ^ World.name ^ "!!")
+type synth
+
+external new_synth : unit -> synth = "PolySynth"
+[@@mel.module "tone"] [@@mel.new]
+
+external to_destination : synth -> unit = "toDestination" [@@mel.send.pipe: synth]
+
+let synth =
+  let synth = new_synth () in
+  to_destination synth;
+  synth
+
